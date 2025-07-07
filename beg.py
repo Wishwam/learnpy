8daee8ebe438a2100eec929ecac3479879bbefc6
Wishwam <wishwam0511@gmail.com>
2025-07-07 18:30:01 +0530

    practised basic python

--- beg.py
+++ beg.py
@@ -42,7 +42,7 @@ print(x is y)
 
 #membership operator #in #notin
 
-# list
+# list[]
 list1 = [1,2,3,"wtf",True]
 print(list1)
 #datatype of list
@@ -83,53 +83,76 @@ list4.sort(reverse=True)
 print(list4)
 
 
-#turple
-# numbers = (1,2,3,4,5)
-# flowers=("rose","lily")
-# print(flowers*3)
-# print(numbers+flowers)
-# print(flowers.index("lily"))
+#turple ()
+tup=("a","b","c")
+print(tup)
+tup1=("Apple","Mango","Banana","Papaya")
+print(tup1[1])
+ #changing(update)a tuple
+x=("Virat","Kane","Smith")
+y=list(x)
+y[2]="Root"
+x=tuple(y)
+print(x)
 
-#turple unpacking
-# person = ("Ben stokes", 36, "cricketer")
-# name,age,profession = person
-# print(name)
+# turple unpacking
+person = ("Ben stokes", 36, "cricketer")
+name,age,profession = person
+print(name)
+print(age)
+print(profession)
+
+#tuple joining
+tup2=("Virat","Root","Smith")
+tup3=("Bumrah","Archer","Cummins")
+tup4=tup2+tup3
+print(tup4)
+tup3=("Bumrah","Archer","Cummins")
+print(tup3*2)
+#indexing
+tup4=("Bumrah","Archer","Cummins","Virat","Root","Smith")
+print(tup4[1:5])
 
 
-#set
-# set1 = {1,2,3,4,5}
-# set2 = ([4,5,6])
-# print(set1)
-# set1.add(10)
-# print(set1)
-# set1.remove(2)
-# set1.discard(6)
-# print(set1)
-# print(10 in set1)
-# print(12 not in set1)
-# set1.update("57")
-# print(set1)
-# set1.pop()
-# print(set1)
+#set{}
+set1 = {1,2,3,4,5}
+set2 = ([4,5,6])
+print(set1)
+print(set2)
+#adding values in set
+set1 = {1,2,3,4,5}
+set1.add("abc")
+print(set1)
 
+set1.update(set2)
+print(set1)
+#adding iterable using update
+set1.update(tup2)
+print(set1)
+#removing items
+set1.remove("abc")
+print(set1)
+#union 
+set3={"Jos","Salt","Tim"}
+set4={"Pat","Wood","Jos"}
+set5=set3|set4
+print(set5)
+set3.update(set4)
+print(set3)
+#symmetric difference-return element that are not present in both set
+set3={"Jos","Salt","Tim"}
+set4={"Pat","Wood","Jos"}
+set5=set3^set4
+print(set5)
+set6={"Jos","Salt","Tim","Jacob"}
+set6.pop()
+print(set6)
+#intersection - return item which is present in both
+set3={"Jos","Salt","Tim"}
+set4={"Pat","Wood","Jos"}
+set=set3&set4
+print(set)
 
-# a={1,2,3,4}
-# b={3,4,5,6}
-#union
-# print(a|b)
-# print(a.union(b))
-
-# #intersection
-# print(a&b)
-# print(a.intersection(b))
-
-#difference
-# print(a-b)
-# print(a.difference(b))
-
-#symmetric difference
-# print(a^b)
-# print(a.symmetric_difference(b))
 
 # square = {x**2 for x in range(1,11)}
 # print(square)

794515f433688051d8f1152540c0759011b082c6
Wishwam <wishwam0511@gmail.com>
2025-07-16 23:40:51 +0530

    palindrome

--- loops.py
+++ loops.py
@@ -137,3 +137,17 @@ def fibonacci(n):
 terms = int(input("Enter the number of terms: "))
 print(f"Fibonacci sequence up to {terms} terms:")
 print(fibonacci(terms))
+
+#palindrome
+given_string = "madam"
+
+reverse_string = ""
+ 
+for i in given_string:
+    reverse_string = i + reverse_string  
+ 
+if(given_string == reverse_string):
+   print("The string", given_string,"is a Palindrome.")
+    
+else:
+   print("The string",given_string,"is NOT a Palindrome.")

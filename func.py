777a92ef2b706ff75ee4b39d23d4f908691099b3
Wishwam <wishwam0511@gmail.com>
2025-07-16 23:35:17 +0530

    recusion

--- func.py
+++ func.py
@@ -41,3 +41,15 @@ def my_function(food):
 fruits = ["apple", "banana", "cherry"]
 
 my_function(fruits)
+
+# tri_recursion
+def tri_recursion(k):
+  if(k > 0):
+    result = k + tri_recursion(k - 1)
+    print(result)
+  else:
+    result = 0
+  return result
+
+print("Recursion Example Results:")
+tri_recursion(6)

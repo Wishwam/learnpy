ef80925d4542eb0bcc373d746b35af708b72cb73
Wish <wishwam0511@gmail.com>
2026-02-04 16:40:05 +0530

    .

---
+++ lab.py
@@ -0,0 +1,13 @@
+import csv
+filename = "data.csv"
+column_name = "Marks"
+total = 0
+count = 0
+with open(filename, "r") as file:
+    reader = csv.DictReader(file)
+    for row in reader:
+        total += float(row[column_name])
+        count += 1
+average = total / count
+print(f"Average of {column_name}: {average}")
+
